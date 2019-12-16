513da9a03c2313212d8cd4f454b84912f34f9e70
Sapto Aji <saosabc321@gmail.com>
2019-12-17 01:16:57 +0700

    tambah keterangan println

--- struktur kontrol/ganjilGenap/GanjilGenap.java
+++ struktur kontrol/ganjilGenap/GanjilGenap.java
@@ -2,7 +2,7 @@ public class GanjilGenap {
     public static void main(String[] args) {
         int nilai = Integer.parseInt(args[0]);
 
-        String hasil = nilai % 2 == 0 ? "Nilai Genap" : "Nilai Ganjil";
-        System.out.println(hasil);
+        String hasil = nilai % 2 == 0 ? "Bilangan Genap" : "Bilangan Ganjil";
+        System.out.println(nilai + " Adalah " + hasil);
     }
 }
