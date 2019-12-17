79da2630ad43c122182ce92a46837755d7b76f45
Sapto Aji <saosabc321@gmail.com>
2019-12-17 15:31:59 +0700

    tambah try catch untuk handling error saat input string

--- struktur kontrol/percabangan/grade/gradeNilai.java
+++ struktur kontrol/percabangan/grade/gradeNilai.java
@@ -1,24 +1,30 @@
 public class gradeNilai {
 
     public static void main(String[] args) {
-        int hasil = Integer.parseInt(args[0]);
-        String grade;
 
-        if (hasil >= 85 && hasil <= 100) {
-            grade = "A";
-            System.out.println("Grade dari nilai " + hasil + " Adalah " + grade);
-        } else if (hasil >= 70 && hasil <= 84) {
-            grade = "B";
-            System.out.println("Grade dari Nilai " + hasil + " Adalah " + grade);
-        } else if (hasil >= 45 && hasil <= 69) {
-            grade = "C";
-            System.out.println("Grade dari Nilai " + hasil + " Adalah " + grade);
-        } else if (hasil >= 0 && hasil <= 44) {
-            grade = "D";
-            System.out.println("Grade dari Nilai " + hasil + " Adalah " + grade);
-            System.out.println("Auto ngulang lu Cok !!!");
-        } else {
-            System.out.println("Nilai yang anda masukkan tidak valid!!");
+        try {
+            int hasil = Integer.parseInt(args[0]);
+            String grade;
+
+            if (hasil >= 85 && hasil <= 100) {
+                grade = "A";
+                System.out.println("Grade dari nilai " + hasil + " Adalah " + grade);
+            } else if (hasil >= 70 && hasil <= 84) {
+                grade = "B";
+                System.out.println("Grade dari Nilai " + hasil + " Adalah " + grade);
+            } else if (hasil >= 45 && hasil <= 69) {
+                grade = "C";
+                System.out.println("Grade dari Nilai " + hasil + " Adalah " + grade);
+            } else if (hasil >= 0 && hasil <= 44) {
+                grade = "D";
+                System.out.println("Grade dari Nilai " + hasil + " Adalah " + grade);
+                System.out.println("Auto ngulang lu Cok !!!");
+            } else {
+                System.out.println("Nilai yang anda masukkan tidak valid!!");
+            }
+        } catch (Exception error) {
+            System.out.println("Harap masukkan Angka !!");
+
         }
     }
 }
